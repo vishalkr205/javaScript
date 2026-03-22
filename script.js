7aeb914ffab603e360b078105c96bfcef0959037
vishal <vishalkrishna205@gmail.com>
2026-03-22 23:58:27 +0530

    frat:basic setup

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+// let str = "vishal krishna";
+// let size=0;
+// for(let i of str) {
+//     console.log("i=",i);
+//     size++;
+// }
+// console.log("size=",size);
+
+// for(let i=1;i<=100;i++) {
+//     if(i%2==0){
+//         console.log("num =",i);
+//     }
+// }
+
+// practice que 2
+// let gameNum = "25";
+// let userNum = prompt("guess the game number : ");
+
+// while(userNum !== gameNum){
+//     userNum = prompt("you entered a wrong game number please enter again");
+
+// }
+// console.log("congretulation you won");
